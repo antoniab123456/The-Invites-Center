0f8f8c7a7a030f64e8337af57b08f7eb0512bc29
Antonia <mac@MacBook-Air.local>
2018-04-25 11:15:38 -0400

    created repository

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+var router = express.Router();
+
+//connecting to the view called index through "/" , getting the home page
+router.get('/', function(req, res){
+    res.render('index');
+  });
+
+  router.get('/invitation', function(req,res){
+    res.render('invitation')
+  });
+
+  module.exports = router;
